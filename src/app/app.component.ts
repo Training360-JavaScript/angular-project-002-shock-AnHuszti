be3d2652080b6cc7494ed2d26f7a3a4b62da33f6
AnHuszti <husztiandrea82@gmail.com>
2022-01-19 03:29:05 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { User } from './model/user';
 import { UserService } from './service/user.service';
 
@@ -9,7 +9,24 @@ import { UserService } from './service/user.service';
 })
 export class AppComponent {
   title = 'The good Angular programmer';
+  
+  constructor(
+    private userService: UserService
+    ) {}
 
-  constructor() {}
+  users: User[] = this.userService.list
+  currentUser: User = new User();
+    
+  onSelectedUserClick(user: User): void {
+    this.currentUser = user;
+  };
+  
+  onUpdatedUserClick(user: User): void {
+    this.userService.updateUser(user)
+  };
+
+  onDeletedUserClick(user: User): void {
+   this.userService.removeUser(user)
+  };
 
 }
